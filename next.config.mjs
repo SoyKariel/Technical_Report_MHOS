46ec4d59324d29e0e014b60a5acd78fa00badc3c
SoyKariel <ok.hernandezserrano@ugto.mx>
2026-02-24 15:06:30 -0600

    Fix: Add Basepath to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   output: 'export',
+
+  basePath: '/Technical_Report_MHOS',
+
+  assetPrefix: '/Technical_Report_MHOS',
+
   images: {
     unoptimized: true,
   },
+
+  reactStrictMode: true,
 };
 
 export default nextConfig;
